07755bcdd91332e366160d01b50c4f70deee6703
ChamorroDev <alexanderchamorro@hotmail.com>
2023-05-04 00:31:40 -0400

    asd

--- TiendaMusical/inicio/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from tienda.models import Disco 
-
-
-def home(request):
-    all_discos=Disco.objects.all()   
- 
-    return render(request,"home.html",{"discos":all_discos})
